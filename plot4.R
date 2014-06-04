6eba143c5d8cf8e1bb3647bbd79eead882bb0d20
pedro pablo malagon amor <pedropabloma@msn.com>
2014-06-04 20:31:37 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,24 +1,39 @@
-# Load RDS
-require(ggplot2)
+#
+# Please refer to the README.md file for an explanation on how I read the original #data
+#
 
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+## Reading Relevent data
+data <- read.table('household_power_consumption.txt', header = T, sep = ';', stringsAsFactors = F, na.strings = '?', skip = 66636, nrow = 2880)
 
-# Coal-related SCC
-SCC.coal = SCC[grepl("coal", SCC$Short.Name, ignore.case=TRUE),]
+colnames(data) <- c('Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')
 
-# Merge two data sets
-merge <- merge(x=NEI, y=SCC.coal, by='SCC')
-merge.sum <- aggregate(merge[, 'Emissions'], by=list(merge$year), sum)
-colnames(merge.sum) <- c('Year', 'Emissions')
+## Merging Date and Time columns into one vector with the appropriate format.
+datetime <- strptime(paste(data$Date, data$Time, sep=' '), format = '%d/%m/%Y %H:%M:%S')
 
-# Across the United States, how have emissions from coal combustion-related sources 
-# changed from 1999-2008?
-png(filename='plot4.png')
-ggplot(data=merge.sum, aes(x=Year, y=Emissions/1000)) + 
-    geom_line(aes(group=1, col=Emissions)) + geom_point(aes(size=2, col=Emissions)) + 
-    ggtitle(expression('Total Emissions of PM'[2.5])) + 
-    ylab(expression(paste('PM', ''[2.5], ' in kilotons'))) + 
-    geom_text(aes(label=round(Emissions/1000,digits=2), size=2, hjust=1.5, vjust=1.5)) + 
-    theme(legend.position='none') + scale_colour_gradient(low='black', high='red')
+## Build a new data frame with only the required data for plot 4
+data <- data.frame(datetime, data[, c(3:5,7:9)])
+
+## Draw the histogram of plot 4 in a png file
+png('plot4.png', bg = 'transparent')
+       ### Set days names to english
+       Sys.setlocale(category = "LC_TIME", locale = "en_GB.UTF-8")
+       par(mfrow=c(2,2))
+
+       with(data,{
+              ### First Graph
+              plot(Global_active_power ~ datetime, type = 'l', xlab = '', ylab = 'Global Active Power')
+       
+              ### Second graph. No need to specify xlabel and ylabel since the defaults are the one needed
+              plot(Voltage ~ datetime, type = 'l')
+       
+              ### Third Graph
+              plot(Sub_metering_1 ~ datetime, col = c('black', 'red', 'blue'), type = 'l', xlab = '', ylab = 'Energy sub metering')
+                     lines(Sub_metering_2 ~ datetime, col = 'red')
+                     lines(Sub_metering_3 ~ datetime, col = 'blue')
+                     legend('topright', bty = 'n', c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c('black', 'red', 'blue'), lty = 1, lwd = 1, cex=0.95)
+
+              ### Fourth Graph. No need to specify xlabel and ylabel since the defaults are the one needed
+              plot(Global_reactive_power ~ datetime, type = 'l')
+              }
+       )
 dev.off()
